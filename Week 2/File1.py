59a34dfcc5eb17ad79317203e3696a5acfe22ebe
aroc-apricity <amauryocortega@gmail.com>
2016-09-16 12:23:26 -0500

    dec to bin conversion

--- Week 2/File1.py
+++ Week 2/File1.py
@@ -4,7 +4,6 @@ Created on 09/09/2016
 
 @author: Amaury Ortega <amauryocortega@gmail.com>
 """
-from builtins import print
 
 ans = 0
 negation_flag = False
@@ -114,3 +113,43 @@ while text!='c':
     else:
         print("Please Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
         continue
+
+# Dec to bin
+num = int(input("Integer number to be converted to binary: "))
+if num < 0:
+    isNeg = True
+    num = abs(num)
+else:
+    isNeg = False
+result = ''
+if num == 0:
+    result = '0'
+while num > 0:
+    result = str(num%2) + result
+    num = num//2
+if isNeg:
+    result = '-' + result
+print('bin: ' + str(result))
+
+# Float Dec to bin
+x = float(input('Enter a decimal number between 0 and 1: '))
+
+p = 0
+while ((2**p)*x)%1 != 0:
+    print('Remainder = '+str((2**p)*x - int((2**p)*x)))
+    p += 1
+
+num = int(x*(2**p))
+
+result = ''
+if num == 0:
+    result = '0'
+while num > 0:
+    result = str(num%2) + result
+    num = num//2
+
+for i in range(p-len(result)):
+    result = '0' + result
+
+result = result[0:-p] + '.' + result[-p:]
+print('The binary representation of the decimal '+ str(x) + ' is ' + result)
