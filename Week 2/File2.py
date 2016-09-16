3c28a5bda8a9027a50e88130e347b8d37fda7cdb
Amaury Ortega <amauryocortega@gmail.com>
2016-09-16 15:09:59 -0500

    Prueba

--- Week 2/File2.py
+++ Week 2/File2.py
@@ -4,4 +4,10 @@ Created on 16/09/2016
 
 @author: Amaury Ortega <amauryocortega@gmail.com>
 """
-
+def is_even(i):
+    """
+    Input: i, a positive int
+    Return True if i is even, otherwise False
+    """
+    print("Hi")
+    return i%2==0
