d1ebc1349d913ee96da88b5442bc391332669d1c
Amaury Ortega <amauryocortega@gmail.com>
2016-09-28 17:12:56 -0500

    Debugging and tested started

--- Week 4/File1.py
+++ Week 4/File1.py
@@ -4,3 +4,31 @@ Created on 28/09/2016
 
 @author: Amaury Ortega <amauryocortega@gmail.com>
 """
+'''
+Defensive programming:
+    docstring = specifications for functions
+    modularize = split program in functions
+    check conditions on input/output
+
+Testing/Validation:
+    prepare input and output to check program
+    how can i break my program
+
+Debugging:
+    which events leads to the error
+    why is not working?
+    how can i fix it?
+
+Setup for testing and debugging:
+    break program into modules
+    document constraints = docstring on modules
+
+Testing:
+    ensure code runs
+    expected result to some inputs
+
+    types:
+        unit testing
+        regression testing = check previous bugs in the same function
+        integration testing = whole program
+'''
